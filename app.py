e690e352c55be763e63d174fe25c92a813f611ab
Eiru <g2342073@tcu.ac.jp>
2025-11-23 04:30:27 +0900

    Update app.py scatter route

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def clear():
     return render_template("index.html", results=[], columns=columns)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
