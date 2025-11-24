eb9549ff8399a7ad0e44f51addfe043ebe876789
Eiru <g2342073@tcu.ac.jp>
2025-11-24 17:55:03 +0900

    update plot.py to use Google Drive CSV

--- plot.py
+++ plot.py
@@ -1,27 +1,22 @@
 import pandas as pd
-import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from flask import current_app
 
-def create_scatter_plot(x_col, y_col):
+def scatter_plot(x_col, y_col, sample_size=8000):
     try:
-        conn = sqlite3.connect("data.db")
-        df = pd.read_sql_query("SELECT * FROM excel_starrydata", conn)
-        conn.close()
-
-        if not x_col or not y_col:
-            return None
-
+        url = "https://drive.google.com/uc?id=1tXaq961lBxWtHi4HV2kKXrfFOVB_ZTjZ"
+        df = pd.read_csv(url)
         df = df[[x_col, y_col]].dropna()
+
 
         if len(df) == 0:
             print("データが空です")
             return None
 
 
-        sample_size = min(8000, len(df))
+        sample_size = min(sample_size, len(df))
         df_sample = df.sample(n=sample_size, random_state=1)
 
         os.makedirs(os.path.join(current_app.root_path, "static"), exist_ok=True)
@@ -33,18 +28,9 @@ def create_scatter_plot(x_col, y_col):
         plt.title(f"{x_col} vs {y_col} (sample {sample_size})", fontsize=16)
         plt.xlabel(x_col, fontsize=14)
         plt.ylabel(y_col, fontsize=14)
-        
+        plt.xticks(rotation=45)
+        plt.show()
 
-        fig_path = os.path.join(
-                current_app.root_path,
-                "static",
-                "scatter_sample.png"
-            )
-        plt.savefig(fig_path)
-        plt.close()
-
-        print("保存先:", fig_path, "存在する？", os.path.exists(fig_path))
-        return fig_path
 
     except Exception as e:
         print("散布図にエラーが発生しました：", e)
